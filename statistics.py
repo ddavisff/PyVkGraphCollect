2fa6495d687998933898361aabae120b3f7aabcd
ddavisff <russian_lion_kennedy@yahoo.com>
2020-01-19 22:58:51 +0300

    modified:   statistics.py

--- statistics.py
+++ statistics.py
@@ -11,6 +11,9 @@ if LOGIN=='' or PASSWORD=='' or DB_LOGIN=='' or DB_PASSWORD=='' or GROUP=='': fr
 import argparse
 import time
 
+import networkx as nx
+
+import matplotlib.pyplot as plt
 
 from vk_api.exceptions import ApiError
 
@@ -114,24 +117,35 @@ class collect:
 
 class merge:
 
-	def UFG(client, group, members=None):
+	def UFG(client, group, non_merged_data=None):
 		#user-friends-group creates intersection database of user's friends who have a subscription to the same group
-
-		if members == None: members = db.read(client, group, '_members')[0:][0]['doc']['members']
-
-		friends = db.read(client, group, '_friends')[0:]
 
 		db.create_database(client, group, '_ufg')
 
-		for row in friends:
-			data = {}
-			data[row['doc']['_id']] = list(set(row['doc']['friends']).intersection(set(members)))
-			db.write(db, client, group, '_ufg', [data])
+		members = db.read(client, group, '_members')[0:][0]['doc']['members']
+		friends_db = db.read(client, group, '_friends')
+
+		if non_merged_data == None: 
+			friends = friends_db[0:]
+
+			for row in friends:
+				data = {}
+				data[row['doc']['_id']] = list(set(row['doc']['friends']).intersection(set(members)))
+				db.write(db, client, group, '_ufg', [data])
+
+		else: 
+			friends = non_merged_data
+
+			for friend_id in friends:
+				data = {}
+				if friends_db[str(friend_id)][0]['doc']['friends'] != 'account is deleted or hidden':
+					data[friends_db[str(friend_id)][0]['doc']['_id']] = list(set(np.asarray(np.array(friends_db[str(friend_id)][0]['doc']['friends']).astype(int))).intersection(set(members)))
+					db.write(db, client, group, '_ufg', [data])
 
 
 class __initialize__:
 
-	def __init__(self, login, password, group, db_login, db_password, merge_type):
+	def __init__(self, login, password, group, db_login, db_password, merge_type, build, collect, plot):
 
 		if login == None: login = LOGIN
 		if password == None: password = PASSWORD
@@ -147,7 +161,10 @@ class __initialize__:
 		try: self.members = db.read(self.client, self.group, '_members')[0][0]['doc']['members']
 		except: self.members = []
 
-		if merge_type == None:
+		self.graph = nx.Graph()
+
+		if collect == True:
+			print('k')
 			self.db_check_friends_and_users()
 			if self.friends_db_length == len(self.members) and self.user_data_db_length == len(self.members):
 				self.make_group_list_record()
@@ -155,23 +172,43 @@ class __initialize__:
 				self.collect()
 				self.make_group_list_record()
 
-		else: 
+		elif merge_type != None: 
 			if merge_type == 'ufg':
 				if self.db_check_merge_type(merge_type) != 0:
 					print(0)
-					members_diff = self.compare_data_with_database('_' + merge_type, self.members, 'id')
-					merge.UFG(self.client, self.group, members_diff)
+					non_merged_data = self.compare_data_with_database('_' + merge_type, self.members, 'id')
+					if len(non_merged_data) != 0: merge.UFG(self.client, self.group, non_merged_data)
 				else: 
 					print(1)
 					merge.UFG(self.client, self.group)
 
 				self.make_group_list_record(merge_type)
+		
+		elif build != None: 
+			print('b')
+			if build == 'ufg': self.build_graph(build)
+
+			print(self.graph.number_of_nodes())
+			clusters_l = [len(c) for c in sorted(nx.connected_components(self.graph), key=len, reverse=True)]
+			print(clusters_l[:20])
+
+			if plot == True:
+				little_v = list(sorted(nx.connected_components(self.graph), key=len, reverse=True)[1])
+				little = self.graph.subgraph(little_v)
+
+				plt.figure()
+				pos = nx.kamada_kawai_layout(graph)
+				nx.draw(little, node_size=200)
+				plt.show()
+
 
 	def compare_data_with_database(self, location, data, doc_value):
 		db_data = []
 		for value in db.read(self.client, self.group, location, docs=False)[0:]: db_data.append(value[doc_value])
 
-		return np.asarray(np.setdiff1d(np.array(data).astype(int), np.array(db_data).astype(int))).tolist()
+		diff = np.asarray(np.setdiff1d(np.array(data).astype(int), np.array(db_data).astype(int))).tolist()
+
+		return diff
 
 	def db_check_friends_and_users(self):
 		try:
@@ -224,21 +261,40 @@ class __initialize__:
 
 			if method not in previous_record:
 				modified_record = previous_record.append(method)		
-				current_record = modified_record
-				current_record.save()
+				previous_record = modified_record
+				previous_record.save()
 
 		check_group_document()
 		if method != None: update_group_list_record(method)
 
+	def build_graph(self, merge_type):
+
+		data = db.read(self.client, self.group, '_' + merge_type)
+		nodes = []
+
+		for user in data[0:]:
+			nodes.append(int(user['doc']['_id']))
+
+		self.graph.add_nodes_from(nodes)
+
+		for user in nodes:
+			for target in data[str(user)][0]['doc']['edges']:
+				self.graph.add_edge(user, int(target))
+
+
 parser = argparse.ArgumentParser(description='VK group and user information collector.')
 
-parser.add_argument('--l', metavar='login', type=str, help='VK login')
-parser.add_argument('--p', metavar='password', type=str, help='VK password')
-parser.add_argument('--g', metavar='group', type=str, help='VK group')
-parser.add_argument('--db_l', metavar='db_login', type=str, help='Database login')
-parser.add_argument('--db_p', metavar='db_password', type=str, help='Database password')
-parser.add_argument('--merge_type', metavar='merge_type', type=str, help='Merge type when merging databases')
+parser.add_argument('-l', '--login', metavar='login', type=str, help='VK login')
+parser.add_argument('-p', '--password', metavar='password', type=str, help='VK password')
+parser.add_argument('-g', '--group', metavar='group', type=str, help='VK group')
+parser.add_argument('-db_l', '--db_login', metavar='db_login', type=str, help='Database login')
+parser.add_argument('-db_p', '--db_password', metavar='db_password', type=str, help='Database password')
+parser.add_argument('-c', '--collect', help='Collect data', action='store_true')
+parser.add_argument('-m', '--merge_type', metavar='merge_type', type=str, help='Merging databases with merge type')
+parser.add_argument('-b', '--build', metavar='build', type=str, help='Build graph')
+parser.add_argument('--plot', help='Plot network', action='store_true')
+
 
 args = parser.parse_args()
 
-__initialize__(login=args.l, password=args.p, group=args.g, db_login=args.db_l, db_password=args.db_p, merge_type=args.merge_type)
+__initialize__(login=args.login, password=args.password, group=args.group, db_login=args.db_login, db_password=args.db_password, collect=args.collect, merge_type=args.merge_type, build=args.build, plot=args.plot)
